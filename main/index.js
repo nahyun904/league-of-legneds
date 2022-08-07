79fb95b18e83eb05a3f3c609d3a6236a6a46dda8
nahyun <sleepingmausoleum@gmail.com>
2022-08-08 06:22:34 +0900

    first commit

---
+++ main/index.js
@@ -0,0 +1,13 @@
+var ImgsizeChange = document.getElementById('brand-logo');
+
+function chengeImg(imgchanger)
+{
+    document.getElementById('brand-logo').src = imgchanger;
+    ImgsizeChange.style.height = '40px';
+}
+
+function chengeImgOut(imgchanger)
+{
+    document.getElementById('brand-logo').src = imgchanger;
+    ImgsizeChange.style.height = '40px';
+}
